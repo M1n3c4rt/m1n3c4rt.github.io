c16d784b8df8fd6f6160299bc16c7799fcbdb5be
m1n3c4rt <vedicbits@gmail.com>
2025-05-27 23:39:36 +0530

    release Bracket Neighbourhood

---
+++ bracketcity-generate.js
@@ -0,0 +1,355 @@
+
+var tree = {"final":"","subhints":[]}
+
+
+function renderInit() {
+
+    var storedTree = decodeURIComponent(document.cookie)
+    try {
+        tree = JSON.parse(storedTree.split("=")[1])
+        const startbutton = document.getElementById("startbutton")
+        startbutton.parentNode.removeChild(startbutton)
+        renderTree()
+        return
+    } catch {
+        console.log("no tree stored!",storedTree)
+    }
+
+    var head = document.createElement("div")
+    head.setAttribute("id","mainHead")
+    head.appendChild(document.createElement("hr"))
+    uplink = document.createElement("a")
+    uplink.setAttribute("class","bracketcitybutton")
+    uplink.setAttribute("id","uplink")
+    uplink.setAttribute("onclick","promptUpload()")
+    uplink.appendChild(document.createTextNode("Upload JSON"))
+    head.appendChild(uplink)
+
+    playlink = document.createElement("a")
+    playlink.setAttribute("class","bracketcitybutton")
+    playlink.setAttribute("id","playlink")
+    playlink.setAttribute("href","bracketcity.html")
+    playlink.appendChild(document.createTextNode("Play puzzles ⇗ "))
+    head.appendChild(playlink)
+
+    document.body.appendChild(head)
+}
+
+function init() {
+    var p = prompt("Enter headline:","")
+    tree.final = p
+    tree.subhints = []
+    const startbutton = document.getElementById("startbutton")
+    startbutton.parentNode.removeChild(startbutton)
+    renderTree()
+}
+
+function promptDownload() {
+    var hashed = hashTree(tree,hashString(tree.final))
+    s = JSON.stringify(hashed)
+    const file = new File([s],"bracket_city_puzzle.json")
+    downloadFile(file)
+}
+
+function promptUpload() {
+    var input = document.createElement("input")
+    input.type = "file"
+    var file = undefined
+    input.onchange = e => {
+        file = e.target.files[0]; 
+
+        if (!file) {
+            alert("No file selected. Please choose a file.", "error");
+            return;
+        }
+
+        if (!file.type.startsWith("application/json")) {
+            alert("Unsupported file type. Please select a JSON file.", "error");
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            var fileText = reader.result;
+            tree = JSON.parse(fileText)
+            tree = hashTree(tree,-hashString(tree.final))
+            try { renderTree() } catch { alert("Error reading the file. Please try again."); renderInit() }
+        };
+        reader.onerror = () => {
+            alert("Error reading the file. Please try again.", "error");
+        };
+        reader.readAsText(file);
+    }
+    input.click();
+}
+
+function hashTree(t,n) {
+    var newTree = structuredClone(t)
+    if ("answer" in newTree) {
+        newTree.answer = caesarCipher(newTree.answer,n)
+    }
+    for (let i = 0; i < t.subhints.length; i++) {
+        newTree.subhints[i] = hashTree(newTree.subhints[i],n)
+    }
+    return newTree
+}
+
+function isClean() {
+    return document.getElementsByClassName("bracketcityerror").length == 0
+}
+
+function renderTree() {
+
+    document.cookie = "editorTree="+JSON.stringify(tree)
+
+    try {
+        document.body.removeChild(document.getElementById("mainHead"))
+    } catch {
+
+    }
+    var head = document.createElement("div")
+    head.setAttribute("id","mainHead")
+    head.appendChild(document.createTextNode(tree.final))
+    head.appendChild(createEditButton([]))
+    head.appendChild(createAddButton([]))
+    
+    if (getBrackets(tree.final).some(x => x < 0 || x >= tree.subhints.length)) {
+        var error = document.createElement('div')
+        error.appendChild(document.createTextNode("Some brackets in this hint ("+String(getBrackets(tree.final).filter(x => x < 0 || x >= tree.subhints.length))+") do not exist! Try adding a bracket."))
+        error.setAttribute("class","bracketcityerror")
+    }
+    
+    duplicates = getBrackets(tree.final).filter((e, i, a) => a.indexOf(e) !== i)
+    if (duplicates.length != 0) {
+        var error = document.createElement('div')
+        error.appendChild(document.createTextNode("Bracket numbers can't appear more than once in the same hint! ("+String(duplicates)+")"))
+        error.setAttribute("class","bracketcityerror")
+    }
+
+    if (duplicates.length != 0 || getBrackets(tree.final).some(x => x < 0 || x >= tree.subhints.length)) {
+        head.appendChild(error)
+        head.appendChild(document.createElement("br"))
+        head.setAttribute("class","bracketcitylistitem bracketcityinvalid")
+    } else {
+        head.setAttribute("class","bracketcitylistitem")
+    }
+
+    head.appendChild(document.createElement("br"))
+    var subList = document.createElement("ol")
+    subList.setAttribute("start",0)
+    for (let i = 0; i < tree.subhints.length; i++) {
+        const element = tree.subhints[i];
+        subList.appendChild(renderSubTree(element,[i],getBrackets(tree.final)))
+    }
+    head.appendChild(subList)
+    document.body.appendChild(head)
+    head.appendChild(document.createElement("hr"))
+    uplink = document.createElement("a")
+    uplink.setAttribute("class","bracketcitybutton")
+    uplink.setAttribute("id","uplink")
+    uplink.setAttribute("onclick","promptUpload()")
+    uplink.appendChild(document.createTextNode("Upload JSON"))
+    head.appendChild(uplink)
+
+    if (isClean()) {
+        downlink = document.createElement("a")
+        downlink.setAttribute("class","bracketcitybutton")
+        downlink.setAttribute("id","downlink")
+        downlink.setAttribute("onclick","promptDownload()")
+        downlink.appendChild(document.createTextNode("Download JSON"))
+        head.appendChild(downlink)
+    }
+
+    playlink = document.createElement("a")
+    playlink.setAttribute("class","bracketcitybutton")
+    playlink.setAttribute("id","playlink")
+    playlink.setAttribute("href","bracketcity.html")
+    playlink.appendChild(document.createTextNode("Play these puzzles ⇗ "))
+    head.appendChild(playlink)
+
+    document.body.appendChild(head)
+}
+
+function renderSubTree(subtree,crumbs,parentbrackets) {
+    var head = document.createElement("li")
+    head.appendChild(document.createTextNode(subtree.answer))
+    sep = document.createElement('span')
+    sep.setAttribute("class","bracketcityseparator")
+    sep.appendChild(document.createTextNode('|'))
+    head.appendChild(sep)
+    head.appendChild(document.createTextNode(subtree.hint))
+    head.appendChild(createEditAnswerButton(crumbs))
+    head.appendChild(createEditHintButton(crumbs))
+    head.appendChild(createRemoveButton(crumbs))
+    head.appendChild(createAddButton(crumbs))
+    head.appendChild(document.createElement("br"))
+
+    if (getBrackets(subtree.hint).some(x => x < 0 || x >= subtree.subhints.length)) {
+        var error = document.createElement('div')
+        error.appendChild(document.createTextNode("Some brackets in this hint ("+String(getBrackets(subtree.hint).filter(x => x < 0 || x >= subtree.subhints.length))+") do not exist! Try adding a bracket."))
+        error.setAttribute("class","bracketcityerror")
+    }
+
+    if (!parentbrackets.includes(crumbs.slice(-1)[0])) {
+        var error = document.createElement('div')
+        error.appendChild(document.createTextNode("This bracket is an orphan! Try adding ["+String(crumbs.slice(-1)[0])+"] somewhere in the parent bracket."))
+        error.setAttribute("class","bracketcityerror")
+    }
+    
+    duplicates = getBrackets(subtree.hint).filter((e, i, a) => a.indexOf(e) !== i)
+    if (duplicates.length != 0) {
+        var error = document.createElement('div')
+        error.appendChild(document.createTextNode("Bracket numbers can't appear more than once in the same hint! ("+String(duplicates)+")"))
+        error.setAttribute("class","bracketcityerror")
+    }
+
+    if (duplicates.length != 0 || !parentbrackets.includes(crumbs.slice(-1)[0]) || getBrackets(subtree.hint).some(x => x < 0 || x >= subtree.subhints.length)) {
+        head.appendChild(error)
+        head.appendChild(document.createElement("br"))
+        head.setAttribute("class","bracketcitylistitem bracketcityinvalid")					
+    } else {
+        head.setAttribute("class","bracketcitylistitem")
+    }
+
+    var subList = document.createElement("ol")
+    subList.setAttribute("start",0)
+    for (let i = 0; i < subtree.subhints.length; i++) {
+        const element = subtree.subhints[i];
+        subList.appendChild(renderSubTree(element,crumbs.concat(i),getBrackets(subtree.hint)))
+    }
+    head.appendChild(subList)
+    return head
+}
+
+function createEditButton(crumbs) {
+    b = document.createElement("a")
+    b.setAttribute("class","bracketcitybutton bracketcitybuttonstart")
+    b.setAttribute("id","edit")
+    b.setAttribute("onclick","edit()")
+    b.appendChild(document.createTextNode("Edit"))
+    return b
+}
+
+function createEditAnswerButton(crumbs) {
+    b = document.createElement("a")
+    b.setAttribute("class","bracketcitybutton bracketcitybuttonstart")
+    b.setAttribute("id","editAnswer-["+String(crumbs)+"]")
+    b.setAttribute("onclick","editAnswer(["+String(crumbs)+"])")
+    b.appendChild(document.createTextNode("Edit Answer"))
+    return b
+}
+
+function createEditHintButton(crumbs) {
+    b = document.createElement("a")
+    b.setAttribute("class","bracketcitybutton")
+    b.setAttribute("id","editHint-["+String(crumbs)+"]")
+    b.setAttribute("onclick","editHint(["+String(crumbs)+"])")
+    b.appendChild(document.createTextNode("Edit Hint"))
+    return b
+}
+
+function createRemoveButton(crumbs) {
+    b = document.createElement("a")
+    b.setAttribute("class","bracketcitybutton")
+    b.setAttribute("id","remove-["+String(crumbs)+"]")
+    b.setAttribute("onclick","remove(["+String(crumbs)+"])")
+    b.appendChild(document.createTextNode("Remove"))
+    return b
+}
+
+function createAddButton(crumbs) {
+    b = document.createElement("a")
+    b.setAttribute("class","bracketcitybutton")
+    b.setAttribute("id","add-["+String(crumbs)+"]")
+    b.setAttribute("onclick","add(["+String(crumbs)+"])")
+    b.appendChild(document.createTextNode("Add Bracket"))
+    return b
+}
+
+function edit() {
+    var n = prompt("New headline:",tree.final)
+    if (n != null) {
+        tree.final = n
+    }
+    renderTree()
+}
+
+function editAnswer(crumbs) {
+    var current = tree
+    for (let i = 0; i < crumbs.length; i++) {
+        const crumb = crumbs[i];
+        current = current.subhints[crumb]
+    }
+    var n = prompt("New answer:",current.answer)
+    if (n != null) {
+        current.answer = n
+    }
+    renderTree()
+}
+
+function editHint(crumbs) {
+    var current = tree
+    for (let i = 0; i < crumbs.length; i++) {
+        const crumb = crumbs[i];
+        current = current.subhints[crumb]
+    }
+    var n = prompt("New hint:",current.hint)
+    if (n != null) {
+        current.hint = n.replaceAll(/(?<=\[)0+(?=\d)/g,"")
+    }
+    renderTree()
+}
+
+function remove(crumbs) {
+    var current = tree
+    for (let i = 0; i < crumbs.length; i++) {
+        const crumb = crumbs[i];
+        if (i == crumbs.length - 1) {
+            current.subhints.splice(crumb,1)
+        } else {
+            current = current.subhints[crumb]
+        }
+    }
+    renderTree()
+}
+
+function add(crumbs) {
+    var current = tree
+    for (let i = 0; i < crumbs.length; i++) {
+        const crumb = crumbs[i];
+        current = current.subhints[crumb]
+    }
+    current.subhints.push({"answer":"Sample answer","hint":"Sample hint","subhints":[]})
+    renderTree()
+}
+
+function getBrackets(str) {
+    try {
+        return [...str.matchAll(/\[\d+\]/g)].flat().map(x => parseInt(x.slice(1,-1)))
+    } catch {
+        return []
+    }
+}
+
+function caesarCipher(s,n) {
+    return s.split("").map(x => String.fromCharCode(x.charCodeAt(0)+n)).join("")
+}
+
+function hashString(s) {
+    return s.split("").map(x => x.charCodeAt(0)).reduce((a,b)=>a+b,0)
+}
+
+function downloadFile(file) {
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.href = URL.createObjectURL(file);
+    link.download = file.name;
+
+    document.body.appendChild(link);
+    link.click();
+
+    setTimeout(() => {
+        URL.revokeObjectURL(link.href);
+        link.parentNode.removeChild(link);
+    }, 0);
+}
---
+++ bracketcity.js
@@ -0,0 +1,286 @@
+
+var tree = {"final":"Welcome to B[0] Neighbour[1]","subhints":[{"answer":"਷ਦਨਰਪਹ","hint":"tennis equipment or annoying cacophony","subhints":[]},{"answer":"ਭ਴਴਩","hint":"a covert operation is under one of these","subhints":[]}]}
+
+var stats = {
+    reveals: 0,
+    peeks: 0,
+    incorrect: 0,
+    keystrokes: 0
+}
+var peeked = []
+var flavortext = ""
+var greentext = {
+    start: 0,
+    end: 0,
+    bracketsbefore: 0,
+    location: [-1]
+}
+var originalHeadline = "this is a [0][1]"
+
+function promptUpload() {
+    var input = document.createElement("input")
+    input.type = "file"
+    var file = undefined
+    input.onchange = e => {
+        file = e.target.files[0]; 
+
+        if (!file) {
+            alert("No file selected. Please choose a file.", "error");
+            return;
+        }
+
+        if (!file.type.startsWith("application/json")) {
+            alert("Unsupported file type. Please select a JSON file.", "error");
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = () => {
+            var fileText = reader.result;
+            tree = JSON.parse(fileText)
+            originalHeadline = tree.final
+            try { renderTree() } catch { alert("Error reading the file. Please try again."); renderInit() }
+        };
+        reader.onerror = () => {
+            alert("Error reading the file. Please try again.", "error");
+        };
+        reader.readAsText(file);
+    }
+    input.click();
+}
+
+function getFillableAnswers(crumbs,t) {
+    var brackets = []
+    if ("final" in t) {
+        brackets = getBrackets(t.final)
+    } else {
+        brackets = getBrackets(t.hint)
+    }
+
+    if (brackets.length == 0) {
+        if ("final" in t) {
+            return []
+        } else {
+            return [[crumbs,caesarCipher(t.answer,-hashString(originalHeadline))]]
+        }
+    } else {
+        var full = []
+        for (let i = 0; i < brackets.length; i++) {
+            const bracket = brackets[i];
+            full = full.concat(getFillableAnswers(crumbs.concat(bracket),t.subhints[bracket]))
+        }
+        return full
+    }
+    
+}
+
+function manageInput(guess) {
+    var matches = getFillableAnswers([],tree).filter(x => x[1].toLowerCase() == guess.toLowerCase())
+    var flavor = document.getElementById("flavor")
+    if (matches.length == 0) {
+        stats.incorrect += 1
+        flavortext = "incorrect!"
+        renderTree()
+    } else {
+        flavortext = "correct!"
+        for (let i = 0; i < matches.length; i++) {
+            const match = matches[i];
+            replaceAnswer(match[0],match[1])
+        }
+    }
+}
+
+function replaceAnswer(crumbs,answer) {
+    var current = tree
+    for (let i = 0; i < crumbs.length; i++) {
+        const crumb = crumbs[i];
+        if (i == crumbs.length - 1) {
+            try {
+                var brackets = getBrackets(current.hint)
+                before = 0
+                for (let i = 0; i < brackets.length; i++) {
+                    const bracket = brackets[i];
+                    if (bracket == crumb) {break} else {before += 1}
+                }
+                greentext.bracketsbefore = before
+
+                greentext.start = current.hint.split(/\[\d+\]/)[before].length
+                greentext.end = greentext.start + answer.length
+                
+                current.hint = current.hint.replace(new RegExp("\\["+String(crumb)+"\\]"),answer)
+                greentext.location = crumbs.slice(0,-1)
+            } catch {
+                var brackets = getBrackets(current.final)
+                before = 0
+                for (let i = 0; i < brackets.length; i++) {
+                    const bracket = brackets[i];
+                    if (bracket == crumb) {break} else {before += 1}
+                }
+                greentext.bracketsbefore = before
+
+                greentext.start = current.final.split(/\[\d+\]/)[before].length
+                greentext.end = greentext.start + answer.length
+                
+                current.final = current.final.replace(new RegExp("\\["+String(crumb)+"\\]"),answer)
+                greentext.location = crumbs.slice(0,-1)
+            }
+        } else {
+            current = current.subhints[crumb]
+        }
+    }
+    renderTree()
+}
+
+function displayScore() {
+    var head = document.createElement("div")
+    head.setAttribute("class","bracketcityresults")
+    var score = 100 - 2*stats.incorrect - 5*stats.peeks - 15*stats.reveals
+    head.appendChild(document.createElement("hr"))
+    head.appendChild(document.createTextNode("Score: "+String(score)+"/100"))
+    head.appendChild(document.createElement("br"))
+    head.appendChild(document.createTextNode("❌ Wrong guesses (-2 per guess): "+String(stats.incorrect)))
+    head.appendChild(document.createElement("br"))
+    head.appendChild(document.createTextNode("👀 Clues peeked (-5 per peek): "+String(stats.peeks)))
+    head.appendChild(document.createElement("br"))
+    head.appendChild(document.createTextNode("🛟 Answers revealed (-10 per reveal): "+String(stats.reveals)))
+    head.appendChild(document.createElement("br"))
+    head.appendChild(document.createTextNode("⌨️ Keystrokes used: "+String(stats.keystrokes)))
+    return head
+}
+
+function renderTree() {
+    
+    try {
+        document.body.removeChild(document.getElementById("head"))
+    } catch {
+
+    }
+    
+    var head = document.createElement("div")
+    head.setAttribute("class","bracketcitymain")
+    head.setAttribute("id","head")
+
+    var input = document.createElement("input")
+    input.setAttribute("type","text")
+    input.setAttribute("placeholder","enter any answer...")
+    input.setAttribute("class","bracketcityinput")
+    input.setAttribute("id","input")
+    input.setAttribute("tabindex",0)
+    input.setAttribute("autofocus","")
+
+    input.addEventListener('keydown', (event) => {
+        if (event.key == 'Enter') {
+            manageInput(input.value)
+            document.getElementById("input").focus()
+        } else {
+            stats.keystrokes += 1
+        }
+    })
+
+    head.appendChild(input)
+
+    var flavor = document.createElement("div")
+    if (flavortext == "correct!") {
+        flavor.setAttribute("class","bracketcityflavor bracketcityflavorcorrect")
+    } else {
+        flavor.setAttribute("class","bracketcityflavor bracketcityflavorincorrect")
+    }
+    flavor.setAttribute("id","flavor")
+    flavor.appendChild(document.createTextNode(flavortext))
+    head.appendChild(flavor)
+    
+    var subBrackets = getBrackets(tree.final)
+    var textParts = tree.final.split(/\[\d+\]/)
+    for (let i = 0; i < textParts.length; i++) {
+        const textPart = textParts[i];
+        if (greentext.bracketsbefore == i && String(greentext.location) == String([])) {
+            head.appendChild(document.createTextNode(textPart.slice(0,greentext.start)))
+            green = document.createElement("span")
+            green.setAttribute("class","bracketcitygreen")
+            green.appendChild(document.createTextNode(textPart.slice(greentext.start,greentext.end)))
+            head.appendChild(green)
+            head.appendChild(document.createTextNode(textPart.slice(greentext.end)))
+        } else {
+            head.appendChild(document.createTextNode(textPart))
+        }
+        if (i != subBrackets.length) {
+            head.appendChild(document.createTextNode("["))
+            const sub = tree.subhints[subBrackets[i]]
+            head.appendChild(renderSubTree(sub,[subBrackets[i]]))
+            head.appendChild(document.createTextNode("]"))
+        }
+    }
+
+    if (subBrackets.length == 0) {
+        head.appendChild(displayScore())
+    }
+
+    document.body.append(head)
+}
+
+function renderSubTree(subtree,crumbs) {
+    var head = document.createElement("span")
+    var subBrackets = getBrackets(subtree.hint)
+    var textParts = subtree.hint.split(/\[\d+\]/)
+    
+    if (subBrackets.length == 0) {
+        head.setAttribute("onclick","processClick(["+String(crumbs)+"])")
+        head.setAttribute("class","bracketcityleaf")
+    }
+
+    for (let i = 0; i < textParts.length; i++) {
+        const textPart = textParts[i];
+        if (greentext.bracketsbefore == i && String(greentext.location) == String(crumbs)) {
+            head.appendChild(document.createTextNode(textPart.slice(0,greentext.start)))
+            green = document.createElement("span")
+            green.setAttribute("class","bracketcitygreen")
+            green.appendChild(document.createTextNode(textPart.slice(greentext.start,greentext.end)))
+            head.appendChild(green)
+            head.appendChild(document.createTextNode(textPart.slice(greentext.end)))
+        } else {
+            head.appendChild(document.createTextNode(textPart))
+        }
+        if (i != subBrackets.length) {
+            head.appendChild(document.createTextNode("["))
+            const sub = subtree.subhints[subBrackets[i]]
+            head.appendChild(renderSubTree(sub,crumbs.concat(subBrackets[i])))
+            head.appendChild(document.createTextNode("]"))
+        }
+    }
+    return head
+}
+
+function processClick(crumbs) {
+    console.log("click processed at: ",crumbs)
+    var current = tree
+    for (let i = 0; i < crumbs.length; i++) {
+        const crumb = crumbs[i];
+        current = current.subhints[crumb]
+    }
+    if (peeked.map(x => String(x)).includes(String(crumbs)) && confirm("You sure you want to reveal this answer?")) {
+        replaceAnswer(crumbs,caesarCipher(current.answer,-hashString(originalHeadline)))
+        stats.reveals += 1
+    } else if (confirm("You sure you want to peek at the first letter of this answer?")) {
+        console.log("adding",crumbs,"to peeked")
+        peeked = peeked.concat([crumbs])
+        current.hint = current.hint.concat(" ("+caesarCipher(current.answer,-hashString(originalHeadline)).toUpperCase()[0]+")")
+        stats.peeks += 1
+    }
+    renderTree()
+}
+
+function getBrackets(str) {
+    try {
+        return [...str.matchAll(/\[\d+\]/g)].flat().map(x => parseInt(x.slice(1,-1)))
+    } catch {
+        return []
+    }
+}
+
+function caesarCipher(s,n) {
+    return s.split("").map(x => String.fromCharCode(x.charCodeAt(0)+n)).join("")
+}
+
+function hashString(s) {
+    return s.split("").map(x => x.charCodeAt(0)).reduce((a,b)=>a+b,0)
+}

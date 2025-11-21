1119143ae5733986966dc64dd296fdfdd50569e0
m1n3c4rt <vedicbits@gmail.com>
2025-11-21 17:07:58 +0530

    release UT/DR leitmotif graph, update homepage

---
+++ index.js
@@ -0,0 +1,15 @@
+addEventListener("resize", (event) => {
+    w = document.getElementsByClassName("stuff")[0].clientWidth
+    console.log(w)
+    document.getElementsByClassName('intro')[0].setAttribute("style",`margin-left: ${(w%440)/2+20}px`)
+})
+w = document.getElementsByClassName("stuff")[0].clientWidth
+document.getElementsByClassName('intro')[0].setAttribute("style",`margin-left: ${(w%440)/2+20}px`)
+
+function updateAgeTime() {
+    console.log(new Date().getTime() - 1191221880000 )
+    console.log((new Date().getTime() - 1191221880000).toString())
+    document.getElementById('agetime').textContent = ((new Date().getTime() - 1191221880000).toString())
+}
+
+setInterval(updateAgeTime,1)
